d844759f10f29ddcdae74f57e778473988a66a9c
plapicola <p.lapicola@gmail.com>
2019-05-28 13:58:35 -0600

    Adds welcome mailer and corrects tests to use appropriate naming

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,8 +1,8 @@
-class Api::V1::WelcomeController
+class Api::V1::WelcomeController < ApplicationController
   def create
-    mail = Mail.new(message_params)
-    if mail.save
-      WelcomeMailer.welcome_message(mail).deliver_later
+    message = Message.new(message_params)
+    if message.save
+      WelcomeMailer.welcome_message(message).deliver_later
       render status: 201, json: {success: "Your message has been sent"}
     else
       render status: 422, json: {error: "Unable to deliver message"}
--- spec/requests/api/v1/welcome_spec.rb
+++ spec/requests/api/v1/welcome_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Welcome API", type: :request do
   describe "Mail Creation" do
     it "Creates and dispatches new mails" do
-      allow_any_instance_of(SurveyMailer).to receive(:new_survey_notice).once
+      allow_any_instance_of(WelcomeMailer).to receive(:welcome_message).once
       post "/api/v1/welcome", params: {
           email: "test@example.com",
           message_name: "Welcome Email",
@@ -15,7 +15,7 @@ RSpec.describe "Welcome API", type: :request do
       expect(response.status).to eq(201)
       expect(result).to be_a Hash
       expect(result).to have_key :success
-      expect(result[:success]).to eq("Your messages have been sent")
+      expect(result[:success]).to eq("Your message has been sent")
     end
   end
 end
