5b94252ac7910c8519b6243be001e4929da1fd7b
plapicola <p.lapicola@gmail.com>
2019-05-28 15:19:07 -0600

    Updates method names on survey mailer following rename

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -1,7 +1,7 @@
 class SurveyMailer < ApplicationMailer
   def new_survey_notice(message)
-    @survey_name = message.survey_name
+    @survey_name = message.message_name
     @user_name = message.user_name
-    mail(to: message.email, subject: "#{message.survey_name} is now available")
+    mail(to: message.email, subject: "#{message.message_name} is now available")
   end
 end
